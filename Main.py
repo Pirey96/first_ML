823286db6bd80308fffe4d5617e4f9b91e723815
Pirey96 <pirey8888@gmail.com>
2020-06-10 10:16:18 -0400

    update

--- Main.py
+++ Main.py
@@ -13,6 +13,7 @@ word=[]
 t_set = []
 new_set = []
 voc = 0
+txt_list_new = []
 #This function gets all the data for 2018
 def get_data():
    Title = data_frame.Title
@@ -28,6 +29,7 @@ story = 0
 ask = 0
 show = 0
 poll = 0
+
 def classify_words():
     # count to keep track of nb of words
     count = 2
@@ -46,6 +48,8 @@ def classify_words():
         #question marks and other chars that would differntiate same words
         i[0] = i[0].lower()
         i[0] = i[0].replace(" ' ","")
+        i[0] = i[0].replace(" '","")
+        i[0] = i[0].replace("' ", "")
         i[0] = i[0].replace("(","")
         i[0] = i[0].replace(")","")
         i[0] = i[0].replace(":","")
@@ -128,11 +132,11 @@ def classify_words():
                             data[8]=data[8]+1
     voc = story+poll+ask+show
     #print ("Story: "+str(story)+'\n'+"ask_hn: "+str(ask)+"\n"+"show_hn: "+str(show)+"\n"+"poll: "+str(poll))
-    smoothed_bayes(story,ask,show,poll)
+    smoothed_bayes()
 
 
 
-def smoothed_bayes(story,ask,show,poll):
+def smoothed_bayes():
     #smoothed conditonal probabilies
     #conditional probabilty variables
     for rows in txt_list:
@@ -155,6 +159,7 @@ def write_to_vocabulary():
         for i in txt_list:
             text_file.write(str(i)+"\n")
 #an array for the test words to be computed
+right = 0
 
 def test_set():
     count = 0
@@ -170,6 +175,8 @@ def test_set():
         for t in t_set:
             t[0] = t[0].lower()
             t[0] = t[0].replace(" ' ","")
+            t[0] = t[0].replace(" '","")
+            t[0] = t[0].replace("' ", "")
             t[0] = t[0].replace("(","")
             t[0] = t[0].replace(")","")
             t[0] = t[0].replace(":","")
@@ -209,7 +216,7 @@ def test_set():
             po = sum(prob_poll)
             highest = max(st, sh, ak, po)
 
-
+            global right
             if highest == st:
                 prediction = 'story'
             elif highest == ak:
@@ -220,6 +227,7 @@ def test_set():
                 prediction = 'poll'
             if prediction == t[1]:
                 correctness = "right"
+                right = right+1
             else:
                 correctness = 'wrong'
             op = []
@@ -246,9 +254,301 @@ def test_set():
                 cpo1 = cpo1+1
     print("Prediction: " + "Story: " + str(cst) + " Show_hn: " + str(csh) + " Ask_hn: " + str(cak)+" Poll: " + str(cpo))
     print("Actual:     " + "Story: " + str(cst1) + " Show_hn: " + str(csh1) + " Ask_hn: " + str(cak1)+" Poll: " + str(cpo1))
+    print("Correct predictions (out 5000): "+str(right))
+    for i in txt_list:
+        txt_list_new.append(i)
 
 
 get_data()
 classify_words()
+temp_story = story
+temp_ask=ask
+temp_show=show
+temp_poll=poll
 write_to_vocabulary()
+
 test_set()
+
+
+
+#####SECTIOn 1.3
+#experiment 1.3.1
+def test_set_exp():
+    count = 0
+    cst= 0
+    cak = 0
+    csh = 0
+    cpo = 0
+    cst1 = 0
+    cak1 = 0
+    csh1= 0
+    cpo1 = 0
+    with open("stopword-result.txt", 'w', encoding='utf-8') as text_file:
+        for t in t_set:
+            t[0] = t[0].lower()
+            t[0] = t[0].replace(" ' ","")
+            t[0] = t[0].replace(" '","")
+            t[0] = t[0].replace("' ", "")
+            t[0] = t[0].replace("(","")
+            t[0] = t[0].replace(")","")
+            t[0] = t[0].replace(":","")
+            t[0] = t[0].replace("?"," ? ")
+            t[0] = t[0].replace(",","")
+            t[0] = t[0].replace(".","")
+            t[0] = t[0].replace("]","")
+            t[0] = t[0].replace("[","")
+            t[0] = t[0].replace("!"," ! ")
+            t[0] = t[0].replace('/'," ")
+            temp = t[0].split()
+            prob_story = []
+            prob_ask = []
+            prob_show = []
+            prob_poll = []
+            prob_story.append(np.math.log10((story) / (story + ask + poll + show)))
+            prob_ask.append(np.math.log10((ask) / (story + ask + poll + show)))
+            prob_show.append(np.math.log10((show) / (story + ask + poll + show)))
+            prob_poll.append(np.math.log10((poll + 0.5 )/ (story + ask + poll + show)))
+            for test_words in temp:
+
+                if test_words in word:
+                    index = txt_list[word.index(test_words)]
+                    prob_story.append(np.math.log10(index[3]))
+                    prob_ask.append(np.math.log10(index[5]))
+                    prob_show.append(np.math.log10(index[7]))
+                    prob_poll.append(np.math.log10(index[9]))
+                else:
+                    continue
+
+            #decides the likely category the given title beliongs to
+            #using a naive bayes classifier and logarithms to avoid
+            #arithmetic underflows
+            st = sum(prob_story)
+            sh = sum(prob_show)
+            ak = sum(prob_ask)
+            po = sum(prob_poll)
+            highest = max(st, sh, ak, po)
+
+            global right
+            if highest == st:
+                prediction = 'story'
+            elif highest == ak:
+                prediction='ask_hn'
+            elif highest == sh:
+                prediction = 'show_hn'
+            else:
+                prediction = 'poll'
+            if prediction == t[1]:
+                correctness = "right"
+                right = right+1
+            else:
+                correctness = 'wrong'
+            op = []
+            count = count+1
+            op.append([count, t[0], prediction, st, ak, sh, po, t[1], correctness])
+            #count the number of predicted stories, polls, asks and shows
+            #and counts number actual news stories to compare
+            text_file.write(str(op)+"\n")
+            if prediction == "story":
+                cst=cst+1
+            if(t[1] == 'story'):
+                cst1 = cst1+1
+            if prediction == "ask_hn":
+                cak=cak+1
+            if t[1] == 'ask_hn':
+                cak1 = cak1+1
+            if prediction == "show_hn":
+                csh=csh+1
+            if t[1] == 'show_hn':
+                csh1 = csh1+1
+            if prediction == "poll":
+                cpo=cpo+1
+            if(t[1] == 'poll'):
+                cpo1 = cpo1+1
+    print("Prediction: " + "Story: " + str(cst) + " Show_hn: " + str(csh) + " Ask_hn: " + str(cak)+" Poll: " + str(cpo))
+    print("Actual:     " + "Story: " + str(cst1) + " Show_hn: " + str(csh1) + " Ask_hn: " + str(cak1)+" Poll: " + str(cpo1))
+    print("Correct predictions (out 5000): "+str(right))
+
+def experiment_1():
+    #obtain all stop words and put them in a list
+    stopwords = open('stopwords.txt','r')
+    global txt_list
+    global story
+    global ask
+    global show
+    global poll
+    count = 0
+    sw = stopwords.readlines()
+    #removing stopwords from the main array
+    for i in sw:
+        i = i.strip()
+        if i in word:
+            story = story - txt_list[word.index(i)][2]
+            ask = ask - txt_list[word.index(i)][4]
+            show = show-txt_list[word.index(i)][6]
+            poll = poll-txt_list[word.index(i)][8]
+
+            del txt_list[word.index(i)]
+            del word [word.index(i)]
+    #write to the correct folder
+    with open("stopword-model.txt", 'w', encoding='utf-8') as text_file:
+        for i in txt_list:
+            count = count+1
+            i[0] = count
+            text_file.write(str(i)+"\n")
+    global right
+    right = 0
+    #reimplement naive bayes using the newly trained algorithm
+    smoothed_bayes()
+    test_set_exp()
+
+
+print("Stopword experiment: ")
+experiment_1()
+
+#EXPERIMENT 1.3.2
+def the_ole_switcheroo():
+    global txt_list
+    txt_list = []
+    for i in txt_list_new:
+        txt_list.append(i)
+    global word
+    word = []
+    for i in txt_list:
+        word.append(i[1])
+    global story
+    story = temp_story
+    global ask
+    ask = temp_ask
+    global show
+    show = temp_show
+    global poll
+    poll = temp_poll
+
+def test_set_exp2():
+    count = 0
+    cst= 0
+    cak = 0
+    csh = 0
+    cpo = 0
+    cst1 = 0
+    cak1 = 0
+    csh1= 0
+    cpo1 = 0
+    with open("stopword-result.txt", 'w', encoding='utf-8') as text_file:
+        for t in t_set:
+            t[0] = t[0].lower()
+            t[0] = t[0].replace(" ' ","")
+            t[0] = t[0].replace(" '","")
+            t[0] = t[0].replace("' ", "")
+            t[0] = t[0].replace("(","")
+            t[0] = t[0].replace(")","")
+            t[0] = t[0].replace(":","")
+            t[0] = t[0].replace("?"," ? ")
+            t[0] = t[0].replace(",","")
+            t[0] = t[0].replace(".","")
+            t[0] = t[0].replace("]","")
+            t[0] = t[0].replace("[","")
+            t[0] = t[0].replace("!"," ! ")
+            t[0] = t[0].replace('/'," ")
+            temp = t[0].split()
+            prob_story = []
+            prob_ask = []
+            prob_show = []
+            prob_poll = []
+            prob_story.append(np.math.log10((story) / (story + ask + poll + show)))
+            prob_ask.append(np.math.log10((ask) / (story + ask + poll + show)))
+            prob_show.append(np.math.log10((show) / (story + ask + poll + show)))
+            prob_poll.append(np.math.log10((poll + 0.5 )/ (story + ask + poll + show)))
+            for test_words in temp:
+
+                if test_words in word:
+                    index = txt_list[word.index(test_words)]
+                    prob_story.append(np.math.log10(index[3]))
+                    prob_ask.append(np.math.log10(index[5]))
+                    prob_show.append(np.math.log10(index[7]))
+                    prob_poll.append(np.math.log10(index[9]))
+                else:
+                    continue
+
+            #decides the likely category the given title beliongs to
+            #using a naive bayes classifier and logarithms to avoid
+            #arithmetic underflows
+            st = sum(prob_story)
+            sh = sum(prob_show)
+            ak = sum(prob_ask)
+            po = sum(prob_poll)
+            highest = max(st, sh, ak, po)
+
+            global right
+            if highest == st:
+                prediction = 'story'
+            elif highest == ak:
+                prediction='ask_hn'
+            elif highest == sh:
+                prediction = 'show_hn'
+            else:
+                prediction = 'poll'
+            if prediction == t[1]:
+                correctness = "right"
+                right = right+1
+            else:
+                correctness = 'wrong'
+            op = []
+            count = count+1
+            op.append([count, t[0], prediction, st, ak, sh, po, t[1], correctness])
+            #count the number of predicted stories, polls, asks and shows
+            #and counts number actual news stories to compare
+            text_file.write(str(op)+"\n")
+            if prediction == "story":
+                cst=cst+1
+            if(t[1] == 'story'):
+                cst1 = cst1+1
+            if prediction == "ask_hn":
+                cak=cak+1
+            if t[1] == 'ask_hn':
+                cak1 = cak1+1
+            if prediction == "show_hn":
+                csh=csh+1
+            if t[1] == 'show_hn':
+                csh1 = csh1+1
+            if prediction == "poll":
+                cpo=cpo+1
+            if(t[1] == 'poll'):
+                cpo1 = cpo1+1
+    print("Prediction: " + "Story: " + str(cst) + " Show_hn: " + str(csh) + " Ask_hn: " + str(cak)+" Poll: " + str(cpo))
+    print("Actual:     " + "Story: " + str(cst1) + " Show_hn: " + str(csh1) + " Ask_hn: " + str(cak1)+" Poll: " + str(cpo1))
+    print("Correct predictions (out 5000): "+str(right))
+def experiment_2():
+    the_ole_switcheroo()
+    global story
+    global ask
+    global show
+    global poll
+    for i in txt_list:
+        if len(i[1])>=9 or len(i[1])<=2:
+
+            story = story-i[2]
+            ask = ask-i[4]
+            show = show-i[6]
+            poll=poll-i[8]
+            txt_list.remove(i)
+
+    for i in word:
+        if len(i)>=9 or len(i)<=2:
+            word.remove(i)
+    count = 0
+    print("Lengthword experiment: ")
+    with open ("wordlength-model.txt","w",encoding="utf-8") as text_file:
+        for i in txt_list:
+            count=count+1
+            i[0]=count
+            text_file.write(str(i)+"\n")
+    global right
+    right = 0
+    smoothed_bayes()
+    test_set_exp2()
+
+experiment_2()
+
+
+#PERFORMANCE EXPERIMENT
